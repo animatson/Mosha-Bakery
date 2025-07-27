53a8fe9104ecc39dd02a9b12e4b41ab78c695df5
Aniseti Sanya <anicetysanya@gmail.com>
2025-07-27 13:26:30 +0300

    all deleted

--- app.py
+++
@@ -1,5 +0,0 @@
-from Bakery import app
-
-
-if __name__ == '__main__':
-     app.run(debug=True) 
--- db.py
+++
@@ -1,6 +0,0 @@
-from Bakery import app, db
-
-# Create the database tables if they do not exist
-with app.app_context():
-    db.create_all()
-    # This ensures that the database is created when the application starts
