dffc80c47eec726131a42140ed21735a3e70580d
Aniseti Sanya <anicetysanya@gmail.com>
2025-07-27 13:03:55 +0300

    database updated

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -6,7 +7,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site3.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY']= "ea34283308ca600a82345b884d250ab3"
 db = SQLAlchemy(app)
@@ -18,8 +19,6 @@ login_manager.login_message_category = "warning"
 from .authetication.routes import auth_bp
 from .users.routes import users
 
-with app.app_context():
-    db.create_all()
 
 # Registering blueprints for authentication and user management
 app.register_blueprint(auth_bp)
